261b890a3075a7c14ec3c215473567ea47e8bed0
Jair Reina <Jair.Reina@digitaslbi.com>
2017-08-16 11:10:59 -0500

    adding routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
+import Project from './components/projects/Projects';
+import Employees from './components/employees/Employees';
+import Timesheets from './components/timesheets/Timesheets';
+
+import {BrowseRouter, Route, Switch, Redirect} from 'react-router-dom';
+
 
 class App extends Component {
-
-  // TODO - actually implement this for realz
-  render() {return (<div />);}
+  render() {
+  	return (
+  		<BrowseRouter>
+  			<div clasName="App">
+  				<Switch>
+  					<Route path="/projects" component={Projects} />
+  					<Route exact path="/employees" component={Employees} />
+  					<Route path="/employees/:user_id/timesheets" components={Timesheets} />
+  					<Redirect to="/employees" />
+  				</Switch>
+  			</div>
+  		</BrowseRouter>
+  	);
+  }
 }
 
 export default App;
