ce359746374dbfdc0fe4003c3b99064005a9d083
Jair Reina <Jair.Reina@digitaslbi.com>
2017-08-16 15:20:10 -0500

    adding redux to employees

--- src/reducers/employee-reducer.js
+++ src/reducers/employee-reducer.js
@@ -1,1 +1,18 @@
-//Placeholder for employee-reducer
+import * as EmployeeActionTypes from '../actions/EmployeeActionTypes';
+
+let employeeReducer = (state = {employees: [], employee:{}}, action) => {
+
+	switch(action.type) {
+		
+		case EmployeeActionTypes.LIST:
+			return Object.assign({}, state, {employees: action.employees });
+
+		case EmployeeActionTypes.GET:
+			return Object.assign({}, state, {employee: action.employee });
+
+		default:
+			return state;
+	}
+}
+
+export default employeeReducer
